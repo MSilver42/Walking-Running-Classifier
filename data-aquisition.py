f1747fb2f14119c39c60a36fffece00a2b82795d
Mason Silver <masonjsilver@gmail.com>
2023-03-27 13:50:41 -0400

    honestly not sure about this one

--- data-aquisition.py
+++ data-aquisition.py
@@ -3,15 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 from sklearn.model_selection import train_test_split
+import warnings
+from tables import NaturalNameWarning
+warnings.filterwarnings('ignore', category=NaturalNameWarning)
 
-hf = h5py.File('data.h5', 'w')
+hdf = pd.HDFStore('data.h5')
 
-g1 = hf.create_group('data')
-g4 = hf.create_group('Mason')
-g5 = hf.create_group('Amy')
-g6 = hf.create_group('Connor')
+temp = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
 
-
+hdf.put('Mason', temp)
+hdf.put('Amy', temp)
+hdf.put('Connor', temp)
 
 # display hf structure 
 def h5_tree(val, pre=''):
@@ -41,7 +43,7 @@ for file in jumping_files:
     if jumping_df.empty:
         jumping_df = temp
     else:
-        jumping_df = jumping_df.append(temp)
+        jumping_df = pd.concat([jumping_df, temp])
 
 walking_df = pd.DataFrame
 walking_files = ['mason-walking-1.csv']
@@ -50,42 +52,21 @@ for file in walking_files:
     if walking_df.empty:
         walking_df = temp
     else:
-        walking_df = walking_df.append(temp)
+        walking_df = pd.concat([walking_df, temp])
 
-rows_per_chunk = int(round(5/(walking_df['Time (s)'][1]-walking_df['Time (s)'][0])))
-
-# take average of each chunk
-# walking_df = walking_df.groupby(np.arange(len(walking_df))//rows_per_chunk)
-# jumping_df = jumping_df.groupby(np.arange(len(jumping_df))//rows_per_chunk)
 
 # add labels
 walking_df['class'] = 0
 jumping_df['class'] = 1
 
-
-print("Walking Dataframe")
-print(walking_df.head())
-print(walking_df.shape)
-print("Jumping Dataframe")
-print(jumping_df.head())
-print(jumping_df.shape)
-
 # combine dataframes
-df = walking_df.append(jumping_df)
+df = pd.concat([walking_df, jumping_df], ignore_index=True)
 
-# split data into train and test
-train, test = train_test_split(df, test_size=0.2, random_state=42)
+# replace column names
+df.columns = ['Time', 'Accel X', 'Accel Y', 'Accel Z', 'Absolute Accel', 'Class']
+
+hdf.put('data', df, format='table', data_columns=True)
+
+print(hdf.keys())
 
 
-
-
-# add train data to h5 file
-hf.create_dataset('data/train', data=train)
-# add test data to h5 file
-hf.create_dataset('data/test', data=test)
-
-with h5py.File('data.h5', 'r') as i:
-    print(i)
-    h5_tree(i)
-
-hf.close()
